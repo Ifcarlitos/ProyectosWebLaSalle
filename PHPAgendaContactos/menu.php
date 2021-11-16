7697a8ff4baf9e41f2267e8518cab8ff2b77409e
Ifcarlitos <cromeromatarin@gmail.com>
2021-11-16 02:31:35 +0100

    funcion editar

--- PHPAgendaContactos/menu.php
+++ PHPAgendaContactos/menu.php
@@ -1,13 +1,10 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
+    <a class="navbar-brand" href="#">Agenda</a>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="index.php">Inicio</a>
+        <li class="nav-link active">
+          <a class="nav-link" href="index.php">Inicio</a>
         </li>
         <li class="nav-link active">
           <a class="nav-link" href="busqueda.php?busqueda=&enviar=">Busqueda</a>
@@ -16,7 +13,7 @@
           <a class="nav-link" href="crea.php">Crear</a>
         </li>
         <li class="nav-link active">
-          <a class="nav-link" href="edita.php">Editar</a>
+          <a class="nav-link" href="edita.php?busqueda=&nombre=&apellido=&telefono=&email=&coment=&enviar=">Editar</a>
         </li>
         <li class="nav-link active">
           <a class="nav-link" href="elimina.php?busqueda=&enviar=">Eliminar</a>
